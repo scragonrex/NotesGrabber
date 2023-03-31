a203ee68ac2cb535b248316b2fad61f704fec477
scragonrex <scragondragon@gmail.com>
2023-03-31 19:36:19 +0530

    Fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,24 +13,32 @@ function Navbar() {
     navigate('/login');
   }
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary" style={{boxShadow:"0px 3px 10px 0px"}}>
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">NotesGrabber</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} aria-current="page" to="/">Home</Link>
-            </li>
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-primary" style={{boxShadow:"0px 3px 10px 0px"}}>
+    //   <div className="container-fluid">
+    //     <Link className="navbar-brand" to="/">NotesGrabber</Link>
+    //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse d-flex align-items-flexen" id="navbarSupportedContent">
+    //       {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    //         <li className="nav-item">
+    //           <Link className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} aria-current="page" to="/">Home</Link>
+    //         </li>
 
-          </ul>
-          {!localStorage.getItem('token') ? <form className="d-flex">
-            <Link className="btn btn-primary mx-1" role="button" to="/login">Login</Link>
-            <Link className="btn btn-primary mx-1" role="button" to="/signup">Signup</Link>
-          </form> : <button onClick={handleLogout} className="btn btn-primary ">Logout</button>}
-        </div>
+    //       </ul> */}
+    //       {!localStorage.getItem('token') ? <form className="d-flex">
+    //         <Link className="btn btn-primary mx-1" role="button" to="/login">Login</Link>
+    //         <Link className="btn btn-primary mx-1" role="button" to="/signup">Signup</Link>
+    //       </form> : <button onClick={handleLogout} className="btn btn-primary ">Logout</button>}
+    //     </div>
+    //   </div>
+    // </nav>
+    <nav class="navbar bg-body-tertiary navbar-dark bg-primary">
+      <div class="container-fluid">
+      <Link className="navbar-brand" to="/">NotesGrabber</Link>
+        {!localStorage.getItem('token') ?
+          <Link className="btn mx-1 btn-primary" role="button" to={`${location.pathname==="/signup"? "/login" : "/signup"}`}>{`${location.pathname==="/signup"? "login" : "signup"}`}</Link>
+       : <button onClick={handleLogout} className="btn btn-primary ">Logout</button>}
       </div>
     </nav>
   )
